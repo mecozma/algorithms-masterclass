d525a79b7b979da0b6e7fff99094c0c6df68f285
mecozma <mecozma@gmail.com>
2021-02-15 16:29:39 +0000

    Priority queues example.

---
+++ data-structures/priority-queues.js
@@ -0,0 +1,83 @@
+class Node {
+    constructor(value, priority) {
+        this.value = value;
+        this.priority = priority;
+    }
+}
+
+class PriorityQueue {
+    constructor() {
+        this.values = [];
+    }
+    enqueue(value, priority) {
+        let newNode = new Node(value, priority);
+        this.values.push(newNode);
+        this.bubbleUp();
+        console.log(this.values);
+    }
+    bubbleUp() {
+        let index = this.values.length - 1;
+        const element = this.values[index];
+        while (index > 0) {
+            let parentIndex = Math.floor((index - 1) / 2);
+            let parent = this.values[parentIndex];
+            if (element.priority >= parent.priority) break;
+            this.values[parentIndex] = element;
+            this.values[index] = parent;
+            index = parentIndex;
+        }
+    }
+    dequeue() {
+        let minValue = this.values[0];
+        let end = this.values.pop();
+        if (this.values.length > 0) {
+            this.values[0] = end;
+            this.sinkDown();
+        }
+        return minValue;
+    }
+    sinkDown() {
+        let index = 0;
+        let length = this.values.length;
+        let element = this.values[0];
+
+        while (true) {
+            let leftChildIndex = 2 * index + 1;
+            let rightChildIndex = 2 * index + 2;
+            let leftChild, rightChild;
+            let swap = null;
+
+            if (leftChildIndex < length) {
+                leftChild = this.values[leftChildIndex];
+                if (leftChild.priority < element.priority) {
+                    swap = leftChildIndex;
+                }
+                if (rightChildIndex < length) {
+                    rightChild = this.values[rightChildIndex];
+                    if (
+                        (swap === null && rightChild.priority < element.priority) ||
+                        (swap !== null && rightChild.priority < leftChild.priority)
+                    ) {
+                        swap = rightChildIndex;
+                    }
+                }
+            }
+            if (swap === null) break;
+            this.values[index] = this.values[swap];
+            this.values[swap] = element;
+            index = swap;
+        }
+    }
+}
+
+let q = new PriorityQueue();
+q.enqueue("Data Structures", 2)
+q.enqueue("Programming basics", 1)
+q.enqueue("Algorithms", 3)
+q.enqueue("os", 4)
+q.enqueue("networking", 5)
+console.log(q.dequeue())
+console.log(q.dequeue())
+console.log(q.dequeue())
+
+console.log(q.values.length)
