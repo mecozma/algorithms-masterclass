660f198b19ee95ce0bd0c2d5c8d8f2c5549816e8
mecozma <mecozma@gmail.com>
2021-02-15 14:55:39 +0000

    Linear search example.

---
+++ searching-algorithms/linear-searching.js
@@ -0,0 +1,15 @@
+/**
+ * Write a function called linearSearch which accepts an array and a value,
+ * and returns the index at which the value exists. If the value does not
+ * exist in the array, return -1.
+ */
+
+function linearSearch(arr, num) {
+    // add whatever parameters you deem necessary - good luck!
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === num) {
+            return i;
+        }
+    }
+    return -1;
+}
