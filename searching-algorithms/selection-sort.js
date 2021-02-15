3c0129463a6fe0930bd08b2bac660fd7db001c60
mecozma <mecozma@gmail.com>
2021-02-15 15:41:16 +0000

    Selection sort example.

---
+++ searching-algorithms/selection-sort.js
@@ -0,0 +1,19 @@
+let unsortedArr = [3, 2, 1, 5, 6, 7, 4];
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        let min = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        if (arr[i] != arr[min]) {
+            let temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+    return arr;
+}
+
+console.log(selectionSort(unsortedArr));
