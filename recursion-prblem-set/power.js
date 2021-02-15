fdbd750f8033714bd4106a235299f6dc8697666c
mecozma <mecozma@gmail.com>
2021-02-15 12:32:14 +0000

    A recursive function that mimic Math.pow().

---
+++ recursion-prblem-set/power.js
@@ -0,0 +1,20 @@
+/*
+Write a function called power which accepts a base and an exponent.
+the function should return the power of the base to the exponent.
+This function should mimic the functionality of Math.pow() - do
+not worry about negative bases and exponents.
+*/
+
+function power(base, exp) {
+    args = [...arguments];
+    if (args.length < 2) {
+        return undefined;
+    }
+    if (args[1] === 0) {
+        return 1
+    }
+    return base * power(base, exp - 1)
+}
+
+
+console.log(power(2, 4))
